9fb2143f5a8891af796d07b3fb65846223c899d9
Laurynsmith12 <laurynsmith555@gmail.com>
2024-07-29 20:29:42 -0600

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,34 +1,32 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"
-import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+ 
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyBEZQR24YwhCbK_Mg4GpEqkoY7CFzd01c8",
+    authDomain: "login-form-c27f0.firebaseapp.com",
+    projectId: "login-form-c27f0",
+    storageBucket: "login-form-c27f0.appspot.com",
+    messagingSenderId: "513177356255",
+    appId: "1:513177356255:web:66ab866fea4e1c9cc5a66a"
+  };
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBEZQR24YwhCbK_Mg4GpEqkoY7CFzd01c8",
-  authDomain: "loginformmode.netlify.app/",
-  projectId: "login-form-c27f0",
-  storageBucket: "login-form-c27f0.appspot.com",
-  messagingSenderId: "513177356255",
-  appId: "1:513177356255:web:66ab866fea4e1c9cc5a66a"
-};
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-function showMessage(message, divId){
+ function showMessage(message, divId){
     var messageDiv=document.getElementById(divId);
     messageDiv.style.display="block";
     messageDiv.innerHTML=message;
     messageDiv.style.opacity=1;
     setTimeout(function(){
         messageDiv.style.opacity=0;
-    },5000); 
-}
-const signup=document.getElementById('submitSignUp');
-signup.addEventListener('click', (event)=>{
+    },5000);
+ }
+ const signUp=document.getElementById('submitSignUp');
+ signUp.addEventListener('click', (event)=>{
     event.preventDefault();
     const email=document.getElementById('rEmail').value;
     const password=document.getElementById('rPassword').value;
@@ -38,23 +36,23 @@ signup.addEventListener('click', (event)=>{
     const auth=getAuth();
     const db=getFirestore();
 
-    createUserWithEmailAndPassword(auth, email, password,firstName,lastName)
+    createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential)=>{
         const user=userCredential.user;
         const userData={
             email: email,
-            password: password,
             firstName: firstName,
             lastName:lastName
         };
         showMessage('Account Created Successfully', 'signUpMessage');
-        const decRef=doc(db, "users", user.uid);
+        const docRef=doc(db, "users", user.uid);
         setDoc(docRef,userData)
         .then(()=>{
             window.location.href='index.html';
         })
         .catch((error)=>{
-            console.error("errror writing document", error);
+            console.error("error writing document", error);
+
         });
     })
     .catch((error)=>{
@@ -66,5 +64,29 @@ signup.addEventListener('click', (event)=>{
             showMessage('unable to create User', 'signUpMessage');
         }
     })
+ });
 
-});
+ const signIn=document.getElementById('submitSignIn');
+ signIn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const auth=getAuth();
+
+    signInWithEmailAndPassword(auth, email,password)
+    .then((userCredential)=>{
+        showMessage('login is successful', 'signInMessage');
+        const user=userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href='homepage.html';
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode==='auth/invalid-credential'){
+            showMessage('Incorrect Email or Password', 'signInMessage');
+        }
+        else{
+            showMessage('Account does not Exist', 'signInMessage');
+        }
+    })
+ })
